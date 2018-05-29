a9624a6e630a506885ab6cfd77390b715a9415a1
agarkovand <Saki03031974>
2018-05-29 22:50:24 +0200

    Initial commit.

---
+++ src/main/java/pl/com/britenet/agarkov/AddressBook/AddressBookApplication.java
@@ -0,0 +1,12 @@
+package pl.com.britenet.agarkov.AddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApplication.class, args);
+	}
+}
